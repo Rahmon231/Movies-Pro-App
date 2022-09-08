3fde9d63ec9df902218ecfa3753cf9abf2123d71
Akanbi Rahmon <23046879+Rahmon231@users.noreply.github.com>
2022-09-08 01:16:08 +0100

    service

--- app/src/main/java/com/lemzeeyyy/movieproapp/model/MovieRepo.java
+++ app/src/main/java/com/lemzeeyyy/movieproapp/model/MovieRepo.java
@@ -1,4 +1,23 @@
 package com.lemzeeyyy.movieproapp.model;
 
+import android.app.Application;
+
+import androidx.lifecycle.MutableLiveData;
+
+import java.util.ArrayList;
+import java.util.List;
+
 public class MovieRepo {
+    private ArrayList<Movie> movies = new ArrayList<>();
+    MutableLiveData<List<Movie>> mutableLiveData = new MutableLiveData<>();
+    private Application application ;
+
+    public MovieRepo(Application application) {
+        this.application = application;
+    }
+
+    public MutableLiveData<List<Movie>> getMutableLiveData() {
+
+        return mutableLiveData;
+    }
 }
