2a4e77c4c70057515da3df0eaaeb1134b303ae6b
Akanbi Rahmon <23046879+Rahmon231@users.noreply.github.com>
2022-09-08 03:11:48 +0100

    movie details activity

--- app/src/main/java/com/lemzeeyyy/movieproapp/MovieDetailsActivity.java
+++ app/src/main/java/com/lemzeeyyy/movieproapp/MovieDetailsActivity.java
@@ -1,14 +1,29 @@
 package com.lemzeeyyy.movieproapp;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
+import androidx.databinding.DataBindingUtil;
 
+import android.content.Intent;
 import android.os.Bundle;
 
+import com.lemzeeyyy.movieproapp.databinding.ActivityMovieDetailsBinding;
+import com.lemzeeyyy.movieproapp.model.Movie;
+
 public class MovieDetailsActivity extends AppCompatActivity {
+    private Movie movie;
+    private ActivityMovieDetailsBinding movieDetailsBinding;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_movie_details);
+        movieDetailsBinding = DataBindingUtil.setContentView(this,R.layout.activity_movie_details);
+        Intent intent = getIntent();
+        if(intent!=null){
+            movie = getIntent().getParcelableExtra("movie");
+            movieDetailsBinding.setMovie(movie);
+            getSupportActionBar().setTitle(movie.getTitle());
+        }
     }
 }
--- app/src/main/java/com/lemzeeyyy/movieproapp/adapter/MovieAdapter.java
+++ app/src/main/java/com/lemzeeyyy/movieproapp/adapter/MovieAdapter.java
@@ -1,4 +1,69 @@
 package com.lemzeeyyy.movieproapp.adapter;
 
-public class MovieAdapter {
+import android.content.Context;
+import android.content.Intent;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import androidx.annotation.NonNull;
+import androidx.databinding.DataBindingUtil;
+import androidx.recyclerview.widget.RecyclerView;
+
+import com.lemzeeyyy.movieproapp.MovieDetailsActivity;
+import com.lemzeeyyy.movieproapp.R;
+import com.lemzeeyyy.movieproapp.databinding.MovieItemLayoutBinding;
+import com.lemzeeyyy.movieproapp.model.Movie;
+
+import java.util.ArrayList;
+
+public class MovieAdapter extends RecyclerView.Adapter<MovieAdapter.MovieViewHolder> {
+    private Context context;
+    private ArrayList<Movie> movieArrayList;
+
+    public MovieAdapter(Context context, ArrayList<Movie> movieArrayList) {
+        this.context = context;
+        this.movieArrayList = movieArrayList;
+    }
+
+    @NonNull
+    @Override
+    public MovieAdapter.MovieViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        MovieItemLayoutBinding movieItemLayoutBinding = DataBindingUtil.inflate(LayoutInflater
+                .from(context), R.layout.movie_item_layout,parent,false);
+        return new MovieViewHolder(movieItemLayoutBinding);
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull MovieAdapter.MovieViewHolder holder, int position) {
+        Movie movie = movieArrayList.get(position);
+        holder.movieItemLayoutBinding.setMovie(movie);
+
+    }
+
+    @Override
+    public int getItemCount() {
+        return movieArrayList.size();
+    }
+
+    public class MovieViewHolder extends RecyclerView.ViewHolder {
+        private MovieItemLayoutBinding movieItemLayoutBinding;
+
+        public MovieViewHolder(@NonNull MovieItemLayoutBinding movieItemLayoutBinding) {
+            super(movieItemLayoutBinding.getRoot());
+            this.movieItemLayoutBinding = movieItemLayoutBinding;
+            movieItemLayoutBinding.getRoot().setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View view) {
+                    int pos = getAdapterPosition();
+                    if(pos != RecyclerView.NO_POSITION){
+                        Movie selectedMovie = movieArrayList.get(pos);
+                        Intent intent = new Intent(context, MovieDetailsActivity.class);
+                        intent.putExtra("movie",selectedMovie);
+                        context.startActivity(intent);
+                    }
+                }
+            });
+        }
+    }
 }
